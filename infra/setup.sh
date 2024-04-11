a0db6892de894c86a018ed713f22770a7c293305
israelnp <israelpereira1984@gmail.com>
2024-04-11 09:25:46 -0300

    fix path at setup file

--- infra/setup.sh
+++ infra/setup.sh
@@ -3,5 +3,5 @@ sudo wget -qO- https://get.docker.com/ | sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/israelnp/trabalho-grafana-prometheus-go-terraform.git
-cd trabalho-grafana-prometheus-go-terraform/app
-docker-compose up -d
+cd /trabalho-grafana-prometheus-go-terraform/app
+sudo docker-compose up -d
